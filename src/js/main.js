a9227002de6d76a47b059399a24d4c5fd465fbaa
Vlad <vladis67948@gmail.com>
2022-05-14 13:59:49 +0300

    practice-0: обращения, переменные, типы информации

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,22 @@
-'use strict';
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const A = prompt("Один из последних просмотренных фильмов?", ""),
+      B = prompt("На сколько оцените его?", ""),
+      C = prompt("Один из последних просмотренных фильмов?", ""),
+      D = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[A] = B;
+personalMovieDB.movies[C] = D;
+
+console.log(personalMovieDB);
 
